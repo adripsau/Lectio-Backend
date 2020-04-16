e95b32fcef6a1be80b7a5e283bb9bea81ae9a2e7
NassrEML <nassr906@gmail.com>
2020-04-16 18:26:37 +0100

    Installation

---
+++ src/main/java/es/ulpgc/LectioBackend/LectioBackendApplication.java
@@ -0,0 +1,13 @@
+package es.ulpgc.LectioBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LectioBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/ulpgc/LectioBackend/LectioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ulpgc.LectioBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
