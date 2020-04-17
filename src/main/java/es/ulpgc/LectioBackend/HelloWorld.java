bf9c847261df6850f3e69ca4e8d5ac7d0684f871
NassrEML <nassr906@gmail.com>
2020-04-17 19:15:10 +0100

    Allow Cross Origin

--- src/main/java/es/ulpgc/LectioBackend/HelloWorld.java
+++ src/main/java/es/ulpgc/LectioBackend/HelloWorld.java
@@ -1,9 +1,11 @@
 package es.ulpgc.LectioBackend;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class HelloWorld {
 
