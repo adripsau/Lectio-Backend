08bcfcd4a8aff6a3bbe2ee4963b5f1c8a4d06cd5
NassrEML <nassr906@gmail.com>
2020-05-07 17:39:58 +0100

    init day

--- src/main/java/es/ulpgc/LectioBackend/LectioBackendApplication.java
+++ src/main/java/es/ulpgc/LectioBackend/LectioBackendApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LectioBackendApplication {
+    
     public static void main(String[] args) {
         SpringApplication.run(LectioBackendApplication.class, args);
     }
